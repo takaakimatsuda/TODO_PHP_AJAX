ce8e7315f2274cc7f76b614a36cc70d81a67e79e
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-11-21 20:53:46 +0900

    DB情報の書き換え

--- config.php
+++ config.php
@@ -2,6 +2,12 @@
 
 ini_set('display_errors', 1);
 
-define('DSN', 'mysql:dbname=todo_app;host=localhost;charset=utf8');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
+$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+$db['dbname'] = ltrim($db['path'], '/');
+$dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+$user = $db['user'];
+$password = $db['pass'];
+
+define('DSN', "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8");
+define('DB_USERNAME', $db['user']);
+define('DB_PASSWORD', $db['pass']);
