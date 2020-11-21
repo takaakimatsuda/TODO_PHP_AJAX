f3a38344f75b9876c8ea3935099d9b23470b0304
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-11-21 21:34:25 +0900

    不要なコードの削除

--- index.php
+++ index.php
@@ -19,7 +19,6 @@ $todos = $todoApp->getAll();
 <head>
   <meta charset="UTF-8">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
   <link rel="stylesheet" href="asset/css/main.scss">
   <title>TO DO LIST</title>
 </head>
