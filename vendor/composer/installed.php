9bff581fa7a0c0ca2dda2dbb1e6a81ca5fc62189
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-11-21 16:55:03 +0900

    composerのインストール

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '53e07cd15c81ad057df19605b5f5e5727a5f7f5c',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '53e07cd15c81ad057df19605b5f5e5727a5f7f5c',
+    ),
+  ),
+);
