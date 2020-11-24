f06b24117ff892ef1a01fb0d11f612990bf8ad76
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-11-24 11:23:42 +0900

    機能コメントとバリデーションの追加

--- Todo.php
+++ Todo.php
@@ -12,25 +12,29 @@ class Todo {
 
     try {
       $this->_db = new \PDO(DSN, DB_USERNAME, DB_PASSWORD);
+      // エラーが発生したときに、PDOExceptionの例外を投げてくれるようになる
       $this->_db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     } catch (\PDOException $e) {
       echo $e->getMessage();
       exit;
     }
   }
-
+  // CSRF対策にトークンを作成
   private function _createToken() {
     if (!isset($_SESSION['token'])) {
       $_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(16));
     }
   }
-
+  
   public function getAll() {
+    // DBから全てのタスクデータを降順に取り出す
     $stmt = $this->_db->query("select * from todos order by id desc");
     return $stmt->fetchAll(\PDO::FETCH_OBJ);
   }
 
   public function post() {
+    // 起動時にバリデーションを呼び出す
+    $this->_validateToken();
 
     if (!isset($_POST['mode'])) {
       throw new \Exception('mode not set!');
@@ -43,6 +47,17 @@ class Todo {
         return $this->_delete();
     }
   }
+
+  private function _validateToken() {
+    if (
+      !isset($_SESSION['token']) ||
+      !isset($_POST['token']) ||
+      $_SESSION['token'] !== $_POST['token']
+    ) {
+      throw new \Exception('invalid token!');
+    }
+  }
+
 
   private function _create() {
     if (!isset($_POST['title']) || $_POST['title'] === '') {
